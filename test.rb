3e185ad6d86cec1b7b033469064e2b94f0822747
Matt Leininger <leiningem001@hawaii.rr.com>
2016-02-19 13:19:13 -1000

    writing / reading to files and serializing

--- test.rb
+++ test.rb
@@ -1,4 +1,36 @@
-# testing
-x = 1
+module Utility
+	require 'yaml'
 
-puts "x + x = #{x + x}"
+	def self.serialize(yaml)
+		YAML::dump(yaml)
+	end
+
+	def self.deserialize(yaml)
+		YAML::load(yaml)
+	end
+
+end
+class Hero
+	def initialize(name, power, health)
+		@name = name
+		@power = power
+		@health = health
+	end
+end
+
+matt = Hero.new("Mattalack", 1000, 2500)
+
+# yaml = Utility::serialize(matt)
+
+# puts yaml
+
+# saves = File.open("saves.txt", "r+") do |file|
+# 	file.puts yaml
+# end
+
+saves = File.open("saves.txt", "r")
+
+yaml = saves.read
+puts "before deserialized: #{yaml}"
+yaml = Utility::deserialize(yaml)
+puts "after deserialized: #{yaml}"
